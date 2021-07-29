c3cfc59ced99287b4b47e5fa1ce6c94de31658ee
Ben Hodges <71901668+Been-H@users.noreply.github.com>
2021-07-29 15:52:55 -0400

    Delete views.py

--- planner_app/planner_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse, reverse_lazy
-import datetime
-
-def landing_view(request, *args, **kwargs):
-    return render(request, 'index.html')
